cac10bbfeaad4319d145ef8433a138826b970da4
tl536 <tl536@scarletmail.rutgers.edu>
2021-02-04 16:54:23 -0500

    finish the apple script and start to work on basket script

--- Assets/01-Apple Picker/Scripts/Apple.cs
+++ Assets/01-Apple Picker/Scripts/Apple.cs
@@ -1,18 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Apple : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static float bottomY = -20f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
