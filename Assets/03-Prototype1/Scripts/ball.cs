b941bf1d69545c3b8f4645b2d06ef2e850a432db
tl536 <tl536@scarletmail.rutgers.edu>
2021-02-18 19:08:05 -0500

    Added collectible goals

---
+++ Assets/03-Prototype1/Scripts/ball.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Goal"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
