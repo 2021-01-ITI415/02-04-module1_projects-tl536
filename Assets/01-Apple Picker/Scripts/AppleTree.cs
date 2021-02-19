eabdab198c83f10f71a29ef2f3a95cea8fd83ab4
tl536 <tl536@scarletmail.rutgers.edu>
2021-02-18 19:39:59 -0500

    Added a score counter

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -8,13 +8,13 @@ public class AppleTree : MonoBehaviour
     public GameObject applePrefab;
 
     // Speed at which the AppleTree moves in meters/second
-    public float speed = 5f;
+    public float speed = 1f;
 
     // Distance where AppleTree turns around
-    public float leftAndRightEdge = 50f;
+    public float leftAndRightEdge = 10f;
 
     // Chance that the AppleTree will change directions
-    public float chanceToChangeDirections = 0.05f;
+    public float chanceToChangeDirections = 0.1f;
 
     // Rate at which Apples will be instantiated
     public float secondsBetweenAppleDrops = 1f;
--- Assets/03-Prototype1/Scripts/ball.cs
+++ Assets/03-Prototype1/Scripts/ball.cs
@@ -1,14 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ball : MonoBehaviour
 {
+    [Header("Set Dynamically")]
+    public Text scoreGT;
+    
+
+    void Start()
+    {
+        GameObject scoreGO = GameObject.Find("ScoreCounter"); // b
+                                                              // Get the Text Component of that GameObject
+        scoreGT = scoreGO.GetComponent<Text>(); // c
+                                                // Set the starting number of points to 0
+        
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Goal"))
         {
             other.gameObject.SetActive(false);
+            int score = int.Parse(scoreGT.text); // d
+                                                 // Add points for catching the apple
+            score += 1;
+            // Convert the score back to a string and display it
+            scoreGT.text = score.ToString();
+
         }
     }
 }
